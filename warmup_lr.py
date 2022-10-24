b87769da62bd6d5e3fcd934410247722a375ad5b
fan84sunny <88661130+fan84sunny@users.noreply.github.com>
2022-10-24 17:34:43 +0800

    add warnup_lr

---
+++ warmup_lr.py
@@ -0,0 +1,38 @@
+import torch
+import numpy as np
+import math
+
+def adjust_learning_rate(optimizer, init_lr, epoch, args, warmup=False):
+    final_lr = float(args.train.final_lr)
+    # Setting  schedule function
+    if warmup:
+        warmup_epochs = args.train.warmup_epochs
+        warmup_lr = args.train.warmup_lr
+        if epoch < warmup_epochs:
+            cur_lr = warmup_lr + (init_lr - warmup_lr) * ((epoch + 1) / warmup_epochs)
+        else:
+            cur_lr = final_lr + (init_lr - final_lr) * 0.5 * (1. + math.cos(math.pi * (epoch - warmup_epochs) / (args.train.num_epochs - warmup_epochs)))
+    else:
+        cur_lr = final_lr + (init_lr - final_lr) * 0.5 * (1. + math.cos(math.pi * epoch / args.train.num_epochs))
+
+    return cur_lr
+
+
+if __name__ == "__main__":
+    # Testing
+    import torchvision
+    from arguments import get_args
+    import matplotlib.pyplot as plt
+
+    model = torchvision.models.resnet50()
+    optimizer = torch.optim.SGD(model.parameters(), lr=999)
+    args = get_args()
+    n_iter = 390
+    
+    lrs = []
+    for epoch in range(args.train.num_epochs):
+        for it in range(n_iter):
+            lr = adjust_learning_rate(optimizer, 0.01, epoch, args)
+            lrs.append(lr)
+    plt.plot(lrs)
+    plt.show()
